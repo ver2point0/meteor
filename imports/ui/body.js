cccbf5927295b3dd714bb45556163b1fe2e145a3
John McIntosh II <jmci10@hotmail.com>
2017-02-19 04:42:41 +0000

    add helpers to body template

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -13,7 +13,12 @@ Template.body.onCreated(function bodyOnCreated() {
 
 Template.body.helpers({
   tasks() {
-    // show newest tasks at the top
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
