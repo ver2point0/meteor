df41cdd2b78f0fd24092a61bba048975dec28009
John McIntosh II <jmci10@hotmail.com>
2017-02-20 03:12:40 +0000

    replace insert with addTask method

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -37,12 +37,7 @@ Template.body.events({
     const text = target.text.value;
     
     // insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
     
     // clear form
     target.text.value = '';
