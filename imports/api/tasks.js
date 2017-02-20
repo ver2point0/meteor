ac04168fa2a944c4ccf78a6c8a72727cd2b945b1
John McIntosh II <jmci10@hotmail.com>
2017-02-20 04:32:14 +0000

    add publication for tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,13 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // this code only runs on the server
+  Meteor.publish('tasks', function taskPUblication() {
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
