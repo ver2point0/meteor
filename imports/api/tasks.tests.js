8530a8a3d93ba9aa72ff82a47a4c48b57157e5c4
John McIntosh II <jmci10@hotmail.com>
2017-02-20 06:07:40 +0000

    add a scaffold for a method test

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,12 @@
+/* esLint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      it('can delete owned task', () => {
+      });
+    });
+  });
+}
